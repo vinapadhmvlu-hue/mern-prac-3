b8bdcd0c5cae6e0df504e4c51338beb45d4ee8dd
vinapadhmvlu-hue <vinapadhmvlu@gmail.com>
2026-07-15 13:29:21 +0530

    Create App component to display employee details

---
+++ 3b.jsx
@@ -0,0 +1,16 @@
+function App() 
+{ 
+ const emp ="vinayak"; 
+ const dept="IT"; 
+ const sal =50000; 
+ return( 
+ <div> 
+ <h1>Employee Details :</h1> 
+ <p>Name : {emp}</p> 
+ <p>Department : {dept}</p> 
+ <p>Salary : {sal}</p> 
+ <h4>vinayak (T029)</h4> 
+ </div> 
+ ); 
+} 
+export default App 
